e6a44b472a0f09e679ff281f79ba86081a86f29b
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-01-10 17:03:36 +0530

    Add the table body comps

---
+++ src/Components/Task/tableBody.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+
+export default function TableBody({ tasks, displayAllTasks, setTasks, setTitleEdit, titleedit, setStatusEdit, statusedit }) {
+  
+  const [editedinput, setEditedInput] = useState('');
+
+  const [bothEdit, setBothEdit] = useState(false);
+
+  const changeCurrentvalue = (e, id) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (editedinput === '') {
+      return;
+    }
+
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, title: editedinput } : task
+    ));
+
+    setEditedInput('');
+    setTitleEdit(null);
+  }
+
+  const optionEdit = (e, id) => {
+    setTasks(tasks.map(task =>
+      task.id === id ? { ...task, status: e.target.value } : task
+    ));
+    setBothEdit(null);
+    setStatusEdit(null);
+  }
+
+  const bothEditFeature = (e, task) => {
+    e.stopPropagation();
+    if (titleedit === task.id && statusedit === task.id) {
+      setStatusEdit(null);
+      setTitleEdit(null);
+      setBothEdit(false);
+    }
+    else {
+      setStatusEdit(task.id);
+      setTitleEdit(task.id);
+      setBothEdit(true);
+    }
+  }
+  return (
+    <tbody>
+      {displayAllTasks.map((task) => (
+        <tr key={task.id} className="border-b border-gray-200 hover:bg-gray-50">
+          <td className="p-4 text-lg cursor-pointer" onClick={() => setTitleEdit(task.id)}>
+            {titleedit === task.id ? (
+              <form
+                onSubmit={(e) => changeCurrentvalue(e, task.id)}
+                className="inline-flex gap-2 items-center bg-blue-100 p-1 rounded shadow"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <input
+                  className="min-w-25.5 px-3 py-1 border border-blue-300 rounded text-black"
+                  placeholder={task.title}
+                  value={editedinput}
+                  onChange={(e) => setEditedInput(e.target.value)}
+                  defaultValue={task.title}
+                />
+                <button
+                  type="submit"
+                  className="px-3 py-1 bg-green-500 text-white rounded text-sm hover:bg-green-600 cursor-pointer"
+                >
+                  Save
+                </button>
+                <button
+                  type="button"
+                  onClick={() => setTitleEdit(null)}
+                  className="px-3 py-1 bg-gray-300 text-gray-700 rounded text-sm hover:bg-gray-400 cursor-pointer"
+                >
+                  Cancel
+                </button>
+              </form>
+            ) : (
+              <span className="hover:text-blue-600 transition-colors">{task.title}</span>
+            )}
+          </td>
+          <td
+            className={`p-4 text-lg flex items-center gap-2 cursor-pointer
+    ${task.status === 'To do' ? 'bg-gray-500' : task.status === 'In Progress' ? 'bg-gray-400' : 'bg-gray-300'}`}
+            onClick={() => setStatusEdit(task.id)} >
+            <span className="w-5xs">{task.status}
+              {statusedit === task.id &&
+                <span className="inline-flex w-4xs  mx-10 gap-2 items-center bg-blue-100 p-1 rounded shadow">
+                  <select onClick={(e) => e.stopPropagation()} className="mx-5 bg-gray-200" onChange={(e) => optionEdit(e, task.id)}>
+                    <option hidden>Select Option</option>
+                    <option>To do</option>
+                    <option>In Progress</option>
+                    <option>Completed</option>
+                  </select>
+                  <button onClick={(e) => (e.stopPropagation(), setStatusEdit(null))} className="px-3 py-1 bg-green-500 text-white rounded text-sm cursor-pointer">Cancel</button>
+                </span>
+              }
+            </span>
+          </td>
+          <td className="w-5">
+            <button onClick={(e) => bothEditFeature(e, task)} className="p-2 m-2 bg-blue-400 rounded-xs cursor-pointer">{bothEdit === false ? 'Edit' : 'Save'}</button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  )
+}
