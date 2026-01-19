75a30457fa57943791120d8c2aea0a5b047df880
Dipesh Sharma <dipeshsharmagautam@outlook.com>
2026-01-19 23:25:15 +0530

    not define a suspense

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { lazy, StrictMode } from 'react'
+import { lazy, StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
